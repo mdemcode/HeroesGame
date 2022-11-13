bd0b60d34e5b01a17a148a9a0f897e65ff7d1824
MDemCode <mdemcode@gmail.com>
2022-11-13 20:02:40 +0100

    Add project files.

---
+++ Model/Gracz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroesGame.Model 
+{
+    internal class Gracz {
+         
+        public Gracz(string imie) {
+            Imie = imie;
+        }
+
+        public string Imie { get; set; }
+        public List<Bohater> Bohaterowie { get; } = new List<Bohater>();
+
+
+    }
+
+}
